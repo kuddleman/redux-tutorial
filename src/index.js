0a315ebe58cab7e13f2cdf662973dafa3e26e71a
Kuddleman <edl1155@gmail.com>
2020-05-29 12:50:28 -0700

    combine reducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { combineReducers, createStore } from 'redux'
+
+
+const productsReducer = ( state = [], action ) => {
+   return state
+}
+
+const userReducer = ( state = '', action ) => {
+  return state
+}
+
+//combine productsReducer and userReducer:
+const allReducers = combineReducers({
+  products: productsReducer,
+  user: userReducer
+})
+
+
+const store = createStore( reducer )
+
+console.log( store.getState())
+
 
 ReactDOM.render(
   <React.StrictMode>
